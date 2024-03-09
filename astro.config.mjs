4a0f4521d49904797b503f16d8d5abc5b4139cb9
nescpro <salazarnestor1996@gmail.com>
2024-03-09 16:55:40 -0500

    Rebuild the project with Astro

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://nesc11.github.io',
+    base: '/four-card-feature-section',
+});
